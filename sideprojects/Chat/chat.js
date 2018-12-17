6b07d1cd54d019140c9c074e29d6491dd7d27e1f
Ravnster <magnus195@yahoo.no>
2018-12-17 12:22:43 +0100

    Update chat.js

--- sideprojects/Chat/chat.js
+++ sideprojects/Chat/chat.js
@@ -1,1 +1,50 @@
-e
+var currentChatRoomText;
+var doc;
+var chatRoomObject;
+
+function showChat() {
+    var mainContentDiv = document.getElementById('mainContent');
+    mainContentDiv.innerHTML = `
+    <div id="chatContainer">
+        <div id="chatfield"> </div>
+        <input type="text" id="txtfield" name="name" >
+        <button onclick="speak()"id="sendbtn">Send</button>
+        </div>
+ 
+    `;
+    initFirebase();
+}
+
+function initFirebase() {
+// Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyA-IgMbAjK5yyOFm_JP12eNeOlpFe1V4kQ",
+        authDomain: "startit-chat.firebaseapp.com",
+        databaseURL: "https://startit-chat.firebaseio.com",
+        projectId: "startit-chat",
+        storageBucket: "startit-chat.appspot.com",
+        messagingSenderId: "611501069118"
+    };
+    firebase.initializeApp(config);
+    var firestore = firebase.firestore();
+    const settings = { timestampsInSnapshots: true };
+    firestore.settings(settings);
+    console.log(1);
+
+    doc = firestore.collection('chat').doc('n1weALkgrn0Qt6MtHu7U');
+    doc.onSnapshot(
+        function(chatRoomSnapshot) {
+            chatRoomObject = chatRoomSnapshot.data();
+            document.getElementById('chatfield').innerHTML = chatRoomObject.text;
+            currentChatRoomText = chatRoomObject.text;
+        });
+}
+
+
+function speak() {
+    var newText = currentChatRoomText + '</br>' + document.getElementById("txtfield").value;
+    
+    doc.set({ text: newText })
+        .then(function (response) { console.log('text sent'); })
+        .catch(function (error) { console.error(error); });
+}
